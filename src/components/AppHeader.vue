34d6e4f0c328527db6e6e896851d6aea81e8aa56
Marco Giammari <marcogiammari@gmail.com>
2023-07-17 19:34:56 +0200

    commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,30 +9,40 @@ export default {
 <template>
     <!-- Header Nav -->
     <section class="bg-white my-0 mx-auto flex justify-center">
-        <nav class="flex justify-between max-w-5xl w-full">
+        <nav class="flex justify-between max-w-5xl w-full h-20">
 
-            <!-- DC Brand Logo -->
+            <!-- Logo -->
             <div class="p-[0.8em]">
                 <a href="#">
-                    <img src="../assets/img/dc-logo.png" alt="dc-logo" class="max-w-[70%]">
+                    <img src="../data/images/logo.png" alt="logo" class="max-w-[70%] max-h-full">
                 </a>
             </div>
 
             <!-- Nav Menu Links -->
             <div class="h-full">
-                <ul class="flex gap-[1em] text-[0.5rem] md:text-xs h-full items-center">
-                    <li v-for="item in navItems">
-                        <a href="#">{{ item.toUpperCase() }}</a>
+                <ul class="flex gap-[1em] text-[0.75rem] md:text-xs h-full items-center">
+                    <li>
+                        <router-link to="/">Home</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/about">About</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/restaurant">Restourant</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/about">Contacts</router-link>
                     </li>
                 </ul>
             </div>
-
         </nav>
     </section>
 
     <!-- Hero Section -->
-    <section class="w-full my-0 mx-auto">
+    <section class="w-full my-0 mx-auto relative">
         <img src="../data/images/sushi-jumbo.jpeg" alt="hero-img" class="max-h-96 w-full object-cover object-top">
+        <button
+            class="border bg-transparent border-primary-orange text-primary-orange px-2 py-1 tracking-wider text-3xl absolute z-10 top-1/2 left-1/2 hover:bg-primary-orange hover:text-white translate-y-[-50%] translate-x-[-50%]">PRENOTA</button>
     </section>
 </template>
 
@@ -42,5 +52,3 @@ export default {
 
 
 
-<!-- <router-link to="/">Go to Home</router-link>
-<router-link to="/about">Go to About</router-link> -->
