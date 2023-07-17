332303475dce7b3a73b7afb61237160244b80ade
soniabergamini <sounia96@outlook.it>
2023-07-17 18:57:36 +0200

    feat: add routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './variables.scss'
+import {router} from './routes'
 import App from './App.vue'
 
 createApp(App).mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppContacts from './components/AppContacts.vue';
+import AppHomepage from './components/AppHomepage.vue';
+
+// 1. Define route components. These can be imported from other files
+// const Homepage = { template: '<div>Homepage</div>' }
+// const Contacts = { template: '<div>Contacts</div>' }
+
+// 2. Define some routes. Each route should map to a component. We'll talk about nested routes later.
+const routes = [
+    { path: '/', component: AppHomepage, name: 'homepage' },
+    { path: '/contacts', component: AppContacts, name: 'contacts' },
+]
+
+// 3. Create the router instance and pass the `routes` option. You can pass in additional options here, but let's keep it simple for now.
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+}) 
