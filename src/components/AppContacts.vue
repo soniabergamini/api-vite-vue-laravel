6ab6c70e43a1550ec5ded383349c7ccf8a809862
Marco Giammari <marcogiammari@gmail.com>
2023-07-17 20:37:56 +0200

    feat: add contacts section + homepage section

--- src/components/AppContacts.vue
+++ src/components/AppContacts.vue
@@ -7,7 +7,18 @@ export default {
 </script>
 
 <template>
-    <div>CONTATTI</div>
+    <section class="w-full my-0 mx-auto relative">
+        <img src="../data/images/map-contacts.jpg" alt="hero-img" class="w-full object-cover object-center max-h-[700px]">
+        <button
+            class="border bg-transparent border-white text-white px-3 py-1.5 tracking-wider text-3xl absolute z-10 top-1/2 left-1/2 hover:bg-primary-orange hover:text-white hover:border-primary-orange translate-y-[-50%] translate-x-[-50%]">CONTATTACI</button>
+        <div class="text-center text-2xl py-16">
+            Tatsumi Ristorante giapponese a Ferrara centro per gustare un ottimo Sushi All You Can Eat
+            Cerchi i migliori ristoranti Ferrara? Ti aspettiamo da Tatsumi, Ristorante cinese e thailandese con il menu All
+            You Can Eat, a pranzo e a cena
+            Vuoi scoprire qualcosa di diverso da osterie, trattorie o ristoranti tipici? Tatsumi, Ristorante centro Ferrara,
+            per mangiare i migliori piatti della cucina giapponese, cinese e thailandese
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
--- src/components/AppHomepage.vue
+++ src/components/AppHomepage.vue
@@ -13,8 +13,26 @@ export default {
         <button
             class="border bg-transparent border-white text-white px-3 py-1.5 tracking-wider text-3xl absolute z-10 top-1/2 left-1/2 hover:bg-primary-orange hover:text-white hover:border-primary-orange translate-y-[-50%] translate-x-[-50%]">PRENOTA</button>
     </section>
-</template>
+    <!-- Intro Restaurants  -->
+    <section class="px-6 py-3 text-center">
+        <h1 class="font-bold text-3xl">Sushi All you can eat a Ferrara? Ristorante Tatsumi</h1>
+        <h3 class="text-2xl mb-3">Il migliore ristorante sushi a Ferrara scegliendo nel menu All you can eat</h3>
+        <p class="leading-8">Tatsumi è il primo dei ristoranti sushi Ferrara centro, a pochi passi dal Castello, conosciuto
+            anche come Castello di San Michele, in una location che offre ai propri ospiti un’esperienza unica, tutta da
+            scoprire. L'ampio locale saprà comodamente accogliervi in un ambiente elegante ed in stile moderno. <br> Il
+            ristorante giapponese a Ferrara, a pochi passi dal Castello Estense, offre ai propri ospiti ricette tradizionali
+            della cucina giapponese. Propone specialità asian fusion e portate innovative create ad arte e con amore. Questi
+            piatti scateneranno una vera tempesta sensoriale che saprà appassionare il vostro gusto! <br> A Tokyo e in tutto
+            il Giappone è tradizione l'arte della cucina ricca di eleganza e dettagli. Come pittori che dipingono su tele
+            bianche, gli chef sapranno accostare le giuste sfumature di colore e sapori, per rendere unica questa
+        esperienza. Nel menu troverete anche la migliore birra giapponese Asahi, Sapporo e il profumato sakè. <br> Cosa
+        si mangia in un ristorante di sushi? Non dovranno mancare: gli antipasti, gli spaghetti di riso sapientemente
+        conditi, i ravioli di carne alla griglia, la tempura ed una sinfonia di sapori con Onighiri, Temaki, Uramaki e
+        carpacci di pesce fresco di prima qualità. <br> Tatsumi: tra i migliori ristoranti giapponesi Ferrara, è aperto
+        sia a pranzo che a cena. Potrete scegliere nel ricco menu sushi all you can eat Ferrara assaporando piatti
+        giapponesi, cinesi, thailandesi ed il sushi a Ferrara, preparati dallo chef con una accurata selezione di
+        ingredienti. Prenota un tavolo per provare la nostra cucina orientale oppure ordina sushi da asporto Ferrara.
+    </p>
+</section></template>
 
-<style lang="scss" scoped>
-@use '../variables.scss' as *;
-</style>
+<style lang="scss" scoped>@use '../variables.scss' as *;</style>
